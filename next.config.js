3e868e0d05a1feeb164001ff0997d912ef275073
dominik.horvath <dominik.horvath@doclerholding.com>
2021-11-25 12:01:58 +0100

    Added purgecss

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["raw.githubusercontent.com", "placedog.net"]
+    domains: ["raw.githubusercontent.com", "placedog.net"],
   },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  }
-}
+  },
+};
